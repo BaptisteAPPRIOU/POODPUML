a9a049c98d33a5021621ef86004319e3cbc4ba98
Abakar-Issa-Ali <abakar-issa-ali@laplateforme.io>
2024-05-22 17:24:33 +0200

    main.cpp modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <raylib.h>
 #include "map.hpp"
+#include "button.hpp"
 #include <iostream>
+
 using namespace std;
 
 int main() {
@@ -28,9 +30,47 @@ int main() {
     camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
     camera.fovy = 50.0f;
 
+    // Load textures for buttons
+    Texture2D buttonTexture1 = LoadTexture("assets/images/button.png");
+    Texture2D buttonHoverTexture1 = LoadTexture("assets/button1_hover.png");
+    Texture2D buttonTexture2 = LoadTexture("assets/images/button.png");
+    Texture2D buttonHoverTexture2 = LoadTexture("assets/button2_hover.png");
+    Texture2D buttonTexture3 = LoadTexture("assets/images/button.png");
+    Texture2D buttonHoverTexture3 = LoadTexture("assets/button3_hover.png");
+    Texture2D buttonTexture4 = LoadTexture("assets/images/button.png");
+    Texture2D buttonHoverTexture4 = LoadTexture("assets/button4_hover.png");
+
+    // Calculate vertical positions to center the buttons
+    int buttonWidth = 300;
+    int buttonHeight = 50;
+    int buttonSpacing = 50;
+    int totalHeight = 4 * buttonHeight + 3 * buttonSpacing;
+    int startY = (screenHeight - totalHeight) / 2;
+
+    // Create buttons
+    Button button1(1550, startY, buttonWidth, buttonHeight, buttonTexture1, buttonHoverTexture1);
+    Button button2(1550, startY + buttonHeight + buttonSpacing, buttonWidth, buttonHeight, buttonTexture2, buttonHoverTexture2);
+    Button button3(1550, startY + 2 * (buttonHeight + buttonSpacing), buttonWidth, buttonHeight, buttonTexture3, buttonHoverTexture3);
+    Button button4(1550, startY + 3 * (buttonHeight + buttonSpacing), buttonWidth, buttonHeight, buttonTexture4, buttonHoverTexture4);
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        // Update buttons
+        Vector2 mousePoint = GetMousePosition();
+        if (button1.IsClicked(mousePoint)) {
+            cout << "Button 1 clicked" << endl;
+        }
+        if (button2.IsClicked(mousePoint)) {
+            cout << "Button 2 clicked" << endl;
+        }
+        if (button3.IsClicked(mousePoint)) {
+            cout << "Button 3 clicked" << endl;
+        }
+        if (button4.IsClicked(mousePoint)) {
+            cout << "Button 4 clicked" << endl;
+        }
+
         // Print the camera position
         cout << "Camera Position: "
              << "x: " << camera.position.x << ", "
@@ -39,16 +79,6 @@ int main() {
         map.checkTileHover(camera);
 
         BeginDrawing();
-            //  int mouseX = GetMouseX();
-            //     int mouseY = GetMouseY();
-            //     if (mouseX >= regionX && mouseX <= regionX + regionWidth &&
-            //         mouseY >= regionY && mouseY <= regionY + regionHeight) {
-            //         // UpdateCamera(&camera, CAMERA_THIRD_PERSON);
-            //         if (IsKeyDown(KEY_RIGHT)) camera.position.x += 1.0f;
-            //         if (IsKeyDown(KEY_LEFT)) camera.position.x -= 1.0f;
-            //         if (IsKeyDown(KEY_UP)) camera.position.y += 1.0f;
-            //         if (IsKeyDown(KEY_DOWN)) camera.position.y -= 1.0f;
-            //     }
             ClearBackground(RAYWHITE);
 
             BeginScissorMode(regionX, regionY, regionWidth, regionHeight);
@@ -65,8 +95,24 @@ int main() {
 
             DrawRectangleLines(regionX, regionY, regionWidth, regionHeight, BLACK);
 
+            // Draw buttons
+            button1.Draw(mousePoint);
+            button2.Draw(mousePoint);
+            button3.Draw(mousePoint);
+            button4.Draw(mousePoint);
+
         EndDrawing();
     }
+
+    // Unload textures
+    UnloadTexture(buttonTexture1);
+    UnloadTexture(buttonHoverTexture1);
+    UnloadTexture(buttonTexture2);
+    UnloadTexture(buttonHoverTexture2);
+    UnloadTexture(buttonTexture3);
+    UnloadTexture(buttonHoverTexture3);
+    UnloadTexture(buttonTexture4);
+    UnloadTexture(buttonHoverTexture4);
 
     CloseWindow();
     return 0;
