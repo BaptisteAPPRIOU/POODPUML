387e133d1cf54ba2bc23713d85764eb365054fa7
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-05-29 13:29:27 +0200

    [change] waves in pause

--- src/wave.cpp
+++ src/wave.cpp
@@ -1,26 +1,26 @@
-#include "wave.hpp"
-#include "enemy.hpp"
-#include <iostream>
-#include <raylib.h>
-#include <vector>
-using namespace std;
+// #include "wave.hpp"
+// #include "enemy.hpp"
+// #include <iostream>
+// #include <raylib.h>
+// #include <vector>
+// using namespace std;
 
-Wave::Wave(const string& enemyType, const Vector3& startPosition, int enemyCount, const vector<Vector2> path)
-    : path(path) {
-    for (int i = 0; i < enemyCount; ++i) {
-        enemies.push_back(Enemy::createEnemy(enemyType, startPosition));
-    }
-}
+// Wave::Wave(const string& enemyType, const Vector3& startPosition, int enemyCount, const vector<Vector2> path)
+//     : path(path) {
+//     for (int i = 0; i < enemyCount; ++i) {
+//         enemies.push_back(Enemy::createEnemy(enemyType, startPosition));
+//     }
+// }
 
-Wave::~Wave() {
-    for (auto enemy : enemies) {
-        delete enemy;
-    }
-}
+// Wave::~Wave() {
+//     for (auto enemy : enemies) {
+//         delete enemy;
+//     }
+// }
 
-void Wave::update() {
-    for (auto enemy : enemies) {
-        enemy->update();
-        enemy->move(path);
-    }
-}
+// void Wave::update() {
+//     for (auto enemy : enemies) {
+//         enemy->update();
+//         enemy->move(path);
+//     }
+// }
--- src/wave.hpp
+++ src/wave.hpp
@@ -1,22 +1,22 @@
-#ifndef WAVE_HPP
-#define WAVE_HPP
+// #ifndef WAVE_HPP
+// #define WAVE_HPP
 
-#include "enemy.hpp"
-#include "pathLoader.hpp"
-#include <vector>
-#include <raylib.h>
-using namespace std;
+// #include "enemy.hpp"
+// #include "pathLoader.hpp"
+// #include <vector>
+// #include <raylib.h>
+// using namespace std;
 
-class Wave {
-public:
-    Wave(const string& enemyType, const Vector3& startPosition, int enemyCount, const vector<Vector2> path);
-    ~Wave();
+// class Wave {
+// public:
+//     Wave(const string& enemyType, const Vector3& startPosition, int enemyCount, const vector<Vector2> path);
+//     ~Wave();
     
-    void update();
+//     void update();
 
-private:
-    vector<Enemy*> enemies;
-    vector<Vector2> path;
-};
+// private:
+//     vector<Enemy*> enemies;
+//     vector<Vector2> path;
+// };
 
-#endif // WAVE_HPP
+// #endif // WAVE_HPP
