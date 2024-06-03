77bd0e12326d291ecac2504438a231d13689853b
baptiste-appriou <baptiste.appriou@laplateforme.io>
2024-06-03 12:07:28 +0200

    [change/creation] Slow tower new textures

--- src/slowTower.cpp
+++ src/slowTower.cpp
@@ -3,8 +3,8 @@
 #include <iostream>
 
 SlowTower::SlowTower(Vector3 position) : Tower(position) {
-    tower = LoadModel("assets/models/tower2.obj");
-    textureTower = LoadTexture("assets/textures/texture_tower2.png");
+    tower = LoadModel("assets/models/slowTower.obj");
+    textureTower = LoadTexture("assets/textures/texture_slowTower.png");
     tower.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textureTower;
     range = 15.0f;
     cost = 100;
